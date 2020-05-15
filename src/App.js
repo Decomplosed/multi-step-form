64a17d4378cb355d933b6e6ff56c4d0a9666a03e
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-15 23:03:47 +0200

    implemented UserForm to the App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import UserForm from './components/UserForm'
 import './App.css'
 
 function App() {
-  return <div className='App'></div>
+  return (
+    <div className='App'>
+      <UserForm />
+    </div>
+  )
 }
 
 export default App
