fa66986e5c8af201145dfc0ee8ed1de1b537b937
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-16 22:46:12 +0200

    moved the code from FormUserDetails to FormPersonalDetails

---
+++ src/components/FormPersonalDetails.js
@@ -0,0 +1,59 @@
+import React, { Component, Fragment } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import AppBar from 'material-ui/AppBar'
+import TextField from 'material-ui/TextField'
+import RaisedButton from 'material-ui/RaisedButton'
+
+class FormPersonalDetails extends Component {
+  continue = (e) => {
+    e.preventDefault()
+    this.props.nextStep()
+  }
+
+  render() {
+    const { values, handleChange } = this.props
+
+    return (
+      <MuiThemeProvider>
+        <Fragment>
+          <AppBar title='Enter User Details' />
+          <TextField
+            hintText='Enter your first name'
+            floatingLabelText='First name'
+            onChange={handleChange('firstName')}
+            defaultValue={values.firstName}
+          />
+          <br />
+          <TextField
+            hintText='Enter your last name'
+            floatingLabelText='Last name'
+            onChange={handleChange('lastName')}
+            defaultValue={values.lastName}
+          />
+          <br />
+          <TextField
+            hintText='Enter your email address'
+            floatingLabelText='Email'
+            onChange={handleChange('email')}
+            defaultValue={values.email}
+          />
+          <br />
+          <RaisedButton
+            label='Continue'
+            primary={true}
+            style={styles.button}
+            onClick={this.continue}
+          />
+        </Fragment>
+      </MuiThemeProvider>
+    )
+  }
+}
+
+const styles = {
+  button: {
+    margin: 15,
+  },
+}
+
+export default FormPersonalDetails
