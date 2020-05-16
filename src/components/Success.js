ca93cf8f7f60d01ffd222f076fe85c19aebf6d3f
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-16 23:09:08 +0200

    removed the Buttons and List

--- src/components/Success.js
+++ src/components/Success.js
@@ -22,28 +22,6 @@ class Success extends Component {
       <MuiThemeProvider>
         <Fragment>
           <AppBar title='Confirm User Data' />
-          <List>
-            <ListItem primaryText='First Name' secondaryText={firstName} />
-            <ListItem primaryText='Last Name' secondaryText={lastName} />
-            <ListItem primaryText='Email' secondaryText={email} />
-            <ListItem primaryText='Occupation' secondaryText={occupation} />
-            <ListItem primaryText='City' secondaryText={city} />
-            <ListItem primaryText='Bio' secondaryText={bio} />
-          </List>
-          <br />
-          <RaisedButton
-            label='Back'
-            primary={false}
-            style={styles.button}
-            onClick={this.back}
-          />
-          <br />
-          <RaisedButton
-            label='Confirm & Continue'
-            primary={true}
-            style={styles.button}
-            onClick={this.continue}
-          />
         </Fragment>
       </MuiThemeProvider>
     )
